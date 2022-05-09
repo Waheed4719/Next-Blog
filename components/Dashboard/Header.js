8c0bd9aaa376e815d6a5858b96ca363a1c0307d8
Waheed Ahmed <dmc4719@gmail.com>
2022-05-09 08:09:48 +0600

    replaced a tags with link tags

--- components/Dashboard/Header.js
+++ components/Dashboard/Header.js
@@ -1,30 +1,28 @@
 import React from 'react'
+import Link from 'next/link'
 
 function Header () {
   return (
     <header className='relative w-[calc(100%-220px)] ml-auto  h-16 bg-gray-200 flex items-center'>
       <div className='w-full mx-auto flex items-center py-4 px-6'>
         <div className='w-1/2'>
-          <a
-            href='/'
-            className='text-gray-600 text-md font-semibold hover:text-gray-800'
-          >
-            Next-blog
-          </a>
+          <Link href='/' passHref>
+            <a className='text-gray-600 text-md font-semibold hover:text-gray-800'>
+              Next-blog
+            </a>
+          </Link>
         </div>
         <div className='w-1/2 text-right'>
-          <a
-            href='/dashboard'
-            className='text-gray-600 text-md font-semibold hover:text-gray-800'
-          >
-            Dashboard
-          </a>
-          <a
-            href='/'
-            className='ml-4 text-gray-600 text-md font-semibold hover:text-gray-800'
-          >
-            About
-          </a>
+          <Link href={'/dashboard'} passHref>
+            <a className='text-gray-600 text-md font-semibold hover:text-gray-800'>
+              Dashboard
+            </a>
+          </Link>
+          <Link passHref href={'/login'}>
+            <a className='ml-4 text-gray-600 text-md font-semibold hover:text-gray-800'>
+              About
+            </a>
+          </Link>
         </div>
       </div>
     </header>
